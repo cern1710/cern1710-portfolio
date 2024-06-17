68963bda53f156bb26e6d42b30930e04626334b8
Samuel Wu <cernunnos1710@gmail.com>
2024-06-17 22:23:19 +0100

    Fix favicon.ico

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -4,12 +4,11 @@ import { Box, Container } from '@chakra-ui/react'
 
 const Main = ({children, router}) => {
     return (
-        <Box as="main" pb={10}>
+        <Box as="main" pb={8}>
             <Head>
-                <meta name="viewport" content="width=device-width, initial-scale=1"/>
-                <meta name="description" content="Samuel's portfolio website"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta name="description" content="Samuel's portfolio website" />
                 <meta name="author" content="Samuel Wu" />
-                <link rel="apple-touch-icon" href="apple-touch-icon.png" />
                 <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
                 <title>Samuel Wu</title>
             </Head>
